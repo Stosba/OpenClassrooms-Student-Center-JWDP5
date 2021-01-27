ed5d72f040b09828bb3c295c853bc69c800a286d
Bastos <bastien.heurtault35@gmail.com>
2021-01-27 21:32:39 +0100

    javaScript janvier

---
+++ produit.js
@@ -0,0 +1,8 @@
+/*Produit*/
+
+// alert
+document.querySelector("#bouton").addEventListener("click", function(){
+    document.querySelector("#alertProduit").style.display = "block";
+});
+
+// produit modif
